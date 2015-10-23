17bde403908d8f407013e849a1686ea72f1cf20b
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-10-23 13:28:00 -0700

    Add search bar, working on styling it

--- app/components/StationList.js
+++ app/components/StationList.js
@@ -6,21 +6,51 @@ var store = require('../store');
 
 module.exports = StationList = React.createClass({
 
+  handleTextInput: function(e) {
+    e.preventDefault();
+    this.setState({
+      searchText: e.target.value
+    });
+  },
+
+  getInitialState: function() {
+    return {
+      searchText: 'Search genres'
+    };
+  },
+
   render: function() {
 
     var stations = this.props.list;
 
+    var color;
+
+    if (this.state.searchText === 'Search genres') {
+      color = {color: 'grey'};
+    } else {
+      color = {};
+    }
+
     return (
       <div className="station-list">
 
-          {
-            stations.map(function(station) {
+        <div className="station-search">
+          <i className="fa fa-search"></i>
+          <form>
+            <input style={color} type="search" value={this.state.searchText} onChange={this.handleTextInput} />            
+          </form>
+        </div>
+
+        {
+          stations.map(function(station) {
+            if (station.name.match(this.state.searchText) || this.state.searchText === 'Search genres') {
               return <Station
                 current={this.props.current === station.name}
                 key={station.name}
                 name={station.name} />
-            }.bind(this))
-          }
+            }
+          }.bind(this))
+        }
 
       </div>
     )
