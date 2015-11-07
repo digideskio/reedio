0014badea564d78ea0ba89bec6a91654b5fc0e2f
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-11-07 15:46:41 -0800

    Add scratchpad for notes about components, store, etc

---
+++ scratchpad.js
@@ -0,0 +1,48 @@
+// App
+
+    // Header
+
+    // Player
+
+        // Youtube
+
+        // PlayerButtons
+
+    // Eq
+
+        // EqHeader
+
+        // EqSlider(s)
+
+
+    // Station List
+
+        // Station(s)
+
+
+    // Footer
+
+
+var store = {
+
+  station: {
+    genre: '',
+    sessionId: ''
+  },
+
+  song: {
+    ytid: ''
+  },
+
+  loading: {
+    station: true,
+    song: true, 
+  },
+
+  stationList: {
+    type: 'all',
+    stations: []
+  },
+  
+};
+
